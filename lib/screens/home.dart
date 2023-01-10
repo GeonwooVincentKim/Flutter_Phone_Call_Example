810040c63802bad4c9b4bf84c52b55c64dd4dc89
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-10 15:46:00 +0900

    [Feature] - Add call feature

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -25,29 +25,66 @@ class _HomeState extends State<Home> {
       navigationBar: const CupertinoNavigationBar(
         middle: Text('Middle')
       ),
-      child: Center(child: buildButton()),
+      child: buildButton()
+      // child: Center(child: buildButton()),
     );
   }
 
   Widget buildButton() {
-    final number = '+821065839661';
+    final number = '<Your Phone Number>';
 
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 12),
-        textStyle: const TextStyle(fontSize: 24),
+    return CupertinoScrollbar(
+      child: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return CupertinoListTile(
+            title: const Text("Vincent's"),
+            onTap: () async {
+              // ignore: deprecated_member_use
+              launch('tel://$number');
+              await FlutterPhoneDirectCaller.callNumber(number);
+            },
+          );
+        },
       ),
-      child: const Text('Call'),
-      onPressed: () async {
-        FlutterPhoneDirectCaller.callNumber(_numberController.text);
-        // final uri = Uri.parse('tel:$number');
-        
-        // if (await canLaunchUrl(uri)) {
-        //   launchUrl(uri);
-        // }
-        // launchUrl('tel://$number' as Uri);
-        // await FlutterPhoneDirectCaller.callNumber(number);
-      },
     );
+    // return ListTile(
+    //   title: Text("Vincent's"),
+    //   subtitle: Text(number),
+    //   onTap: () async {
+    //     // ignore: deprecated_member_use
+    //     launch('tel://$number');
+
+    //     await FlutterPhoneDirectCaller.callNumber(number);
+    //   },
+    // );
+
+   
+    // return CupertinoListTile(
+    //   title: Text("Vincent's"),
+    //   onTap: () async {
+    //     // ignore: deprecated_member_use
+    //     launch('tel://$number');
+    //     await FlutterPhoneDirectCaller.callNumber(number);
+    //   },
+    // );
+
+    // return ElevatedButton(
+    //   style: ElevatedButton.styleFrom(
+    //     padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 12),
+    //     textStyle: const TextStyle(fontSize: 24),
+    //   ),
+    //   child: const Text('Call'),
+    //   onPressed: () async {
+    //     FlutterPhoneDirectCaller.callNumber(_numberController.text);
+    //     // final uri = Uri.parse('tel:$number');
+        
+    //     // if (await canLaunchUrl(uri)) {
+    //     //   launchUrl(uri);
+    //     // }
+    //     // launchUrl('tel://$number' as Uri);
+    //     // await FlutterPhoneDirectCaller.callNumber(number);
+    //   },
+    // );
   }
 }
